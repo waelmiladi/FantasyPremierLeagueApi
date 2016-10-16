57504c995b06ab8eaeb3ce16da0bf90dcca222ea
Waheed El Miladi <waheed@intercom.io>
2016-10-16 12:47:04 +0100

    Add player classes

---
+++ src/main/java/com/waheed/fantasy_premier_league_api/players/Forward.java
@@ -0,0 +1,10 @@
+package com.waheed.fantasy_premier_league_api.players;
+
+/**
+ * Created by waheedelmiladi on 16/10/2016.
+ */
+public class Forward extends PlayerType {
+    public Forward(int id, String singularName, String singularNameShort, String pluralName, String pluralNameShort) {
+        super(id, singularName, singularNameShort, pluralName, pluralNameShort);
+    }
+}
